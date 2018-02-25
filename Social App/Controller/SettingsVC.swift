72e0fd0d1e92a1e252dcb94199199a70b11677b9
C Mayank Dogra <cmayankdogra@Mayanks-Macbook.local>
2018-02-25 12:10:35 +0530

    Updated settings VC

--- Social App/Controller/SettingsVC.swift
+++ Social App/Controller/SettingsVC.swift
@@ -14,12 +14,33 @@ class SettingsVC: UIViewController, UIImagePickerControllerDelegate, UINavigatio
     @IBOutlet weak var profileImg: UIImageView!
     @IBOutlet weak var usernameLbl: FancyField!
     
+    var imagePicker: UIImagePickerController!
+    var imageSelected = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
     }
     
     @IBAction func backBtnTapped(_ sender: Any) {
         performSegue(withIdentifier: segueToFeeds, sender: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            profileImg.image = image
+            imageSelected = true
+        } else {
+            print("MD: A valid image wasn't selected!!!")
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func imgEdit(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
     }
     
     
